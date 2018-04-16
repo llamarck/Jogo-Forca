8a170795e01e8bdf0bbbc8ded1922acf5d245788
Leonis Lamarck <lamarck.ti@gmail.com>
2018-04-15 22:11:20 -0300

    Concepção

--- main.c
+++ main.c
@@ -6,8 +6,28 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 int main(){
+	char guess;
+	int hit = 0;
+	int hang = 0;
 	char secret_word[20];
+
+	do {
+		char chute;
+		printf("Chute uma letra: ");
+		fflush(stdout);
+		scanf("%c", guess);
+		fflush(stdout);
+
+		for(int i = 0; i <= strln(secret_word); i++){
+			if(secret_word[i] = guess){
+
+			}
+		}
+
+	} while(!hit && !hang);
+
 	return 0;
 }
