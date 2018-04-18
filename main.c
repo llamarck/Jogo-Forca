9a08cbc22eeaac309a19aa47deca2e44593c1451
Leonis Lamarck <lamarck.ti@gmail.com>
2018-04-17 22:06:11 -0300

    Correção de loops encadeados

--- main.c
+++ main.c
@@ -9,25 +9,58 @@
 #include <string.h>
 
 int main(){
-	char guess;
+
+	int attempts = 0;
 	int hit = 0;
 	int hang = 0;
-	char secret_word[20];
+
+	char guess;
+	char all_guesses[30];
+	char secret_word[30];
+
+	sprintf(secret_word, "developer");
+	fflush(stdout);
+
+	int i;
+	int j;
 
 	do {
-		char chute;
-		printf("Chute uma letra: ");
-		fflush(stdout);
-		scanf("%c", guess);
-		fflush(stdout);
+		for (i = 0; i < strlen(secret_word); i++){
+			int found = 0;
 
-		for(int i = 0; i <= strln(secret_word); i++){
-			if(secret_word[i] = guess){
+			for (j = 0; j < attempts; j++){
+				if(all_guesses[j] == secret_word[i]){
+					found = 1;
+					break;
+				}
+			}
 
+			if(found){
+				printf("%c ", secret_word[i]);
+			}else{
+				printf("_ ");
 			}
 		}
 
-	} while(!hit && !hang);
+		printf("\n");
+		fflush(stdout);
+
+		printf("Chute uma letra: ");
+		fflush(stdout);
+		scanf(" %c", &guess);
+		fflush(stdout);
+
+		all_guesses[attempts] = guess;
+		attempts++;
+
+		/*for(; i < strlen(secret_word); i++){ //diz a posição e se existe a letra!
+			if(secret_word[i] == guess){
+				printf("A posição %d tem esta letra!\n", i+1);
+				fflush(stdout);
+			}
+		}*/
+
+	} while(!hit && !hang); //executa isto enquanto não tiver acertado ou sido enforcado
 
 	return 0;
 }
