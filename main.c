2d9617dcc4c2dff15afe491083b1ab5f83a3f8fe
Leonis Lamarck <lamarck.ti@gmail.com>
2018-04-20 14:22:18 -0300

    Modularização (extraindo mais funções); correção de bugs no buffer do scanf; e inserção experimental de ponteiros

--- main.c
+++ main.c
@@ -12,60 +12,69 @@ void opening(){
 		printf("/*****************/\n");
 		printf("|~ Jogo da Forca ~|\n");
 		printf("/*****************/\n\n");
-	}
+}
 
-void guessing(char guesses[], int attempts){
-	attempts = 0;
+void choose_word(char * secret_word){
+	sprintf(secret_word, "developer");
+}
+
+void guessing(char guesses[], int *attempts){
 	char guess;
 	//guesses[30];
 	printf("Chute uma letra: ");
 	fflush(stdout);
 	scanf(" %c", &guess);
 	fflush(stdout);
-	guesses[attempts] = guess;
-	attempts++;
+	guesses[*attempts] = guess;
+	(*attempts)++;
+}
+
+int alreadyGuessed(char letter, char * guesses, int attempts){
+	int j = 0;
+	int found = 0;
+	for (; j < attempts; j++){
+		if(guesses[j] == letter){
+			found = 1;
+			break;
+		}
+	}
+	return found;
+}
+
+void draw(char * secret_word, char * all_guesses, int attempts){
+	int i;
+	printf("Você já deu %d chutes!\n", attempts);
+	fflush(stdout);
+	for (i = 0; i < strlen(secret_word); i++){
+
+				if(alreadyGuessed(secret_word[i], all_guesses, attempts)){
+					printf("%c ", secret_word[i]);
+				}else{
+					printf("_ ");
+				}
+			}
+
+	printf("\n");
 }
 
 int main(){
 
-	opening();
 
+	int found = 0;
 	int hit = 0;
 	int hang = 0;
 	int attempts = 0;
-	char guess;
+	//char guess;
 	char all_guesses[30];
 	char secret_word[30];
 
-	sprintf(secret_word, "developer");
-	fflush(stdout);
+	opening();
+	choose_word(secret_word);
 
-	int i;
-	int j;
 
 	do {
-		for (i = 0; i < strlen(secret_word); i++){
-			int found = 0;
-
-			for (j = 0; j < attempts; j++){
-				if(all_guesses[j] == secret_word[i]){
-					found = 1;
-					break;
-				}
-			}
-
-			if(found){
-				printf("%c ", secret_word[i]);
-			}else{
-				printf("_ ");
-			}
-		}
-
-		printf("\n");
-		fflush(stdout);
-
-		guessing(all_guesses, attempts);
-
+		draw(secret_word, all_guesses, attempts);
+		guessing(all_guesses, &attempts);
 
 		/*for(; i < strlen(secret_word); i++){ //diz a posição e se existe a letra!
 			if(secret_word[i] == guess){
